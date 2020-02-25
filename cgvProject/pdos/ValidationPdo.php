3e47dc168d05161eff5e30e22d9b38144c3db543
100winone <53801551+100winone@users.noreply.github.com>
2020-02-25 12:33:58 +0900

    5th commit

--- cgvProject/pdos/ValidationPdo.php
+++ cgvProject/pdos/ValidationPdo.php
@@ -39,3 +39,104 @@ function isMovie($movieId)
     return intval($res[0]["exist"]);
 }
 
+
+function isTheater($theaterID)
+{
+    $pdo = pdoSqlConnect();
+    $query = "SELECT EXISTS(
+                            SELECT * 
+                              FROM theater 
+                             WHERE theaterId = ?) AS exist;";
+
+    $st = $pdo->prepare($query);
+    $st->execute([$theaterID]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st = null;
+    $pdo = null;
+
+    return intval($res[0]["exist"]);
+}
+
+function isMovieTime($MovieTimeId)
+{
+    $pdo = pdoSqlConnect();
+    $query = "SELECT EXISTS(
+                            SELECT * 
+                              FROM current_movies
+                             WHERE id = ?) AS exist;";
+
+    $st = $pdo->prepare($query);
+    $st->execute([$MovieTimeId]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st = null;
+    $pdo = null;
+
+    return intval($res[0]["exist"]);
+}
+
+function compareCurDate($movieTimeId)
+{
+    $pdo = pdoSqlConnect();
+    $query = "SELECT EXISTS(
+                            SELECT *
+                              FROM current_movies
+                             WHERE id = ?
+                               AND CURDATE() < date) AS exist";
+
+    $st = $pdo->prepare($query);
+    $st->execute([$movieTimeId]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st = null;
+    $pdo = null;
+
+    return intval($res[0]["exist"]);
+}
+
+function compareEqualDate($movieTimeId)
+{
+    $pdo = pdoSqlConnect();
+    $query = "SELECT EXISTS(
+                            SELECT *
+                              FROM current_movies
+                             WHERE id = ?
+                               AND CURDATE() = date) AS exist";
+
+    $st = $pdo->prepare($query);
+    $st->execute([$movieTimeId]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st = null;
+    $pdo = null;
+
+    return intval($res[0]["exist"]);
+}
+
+function compareCurTime($movieTimeId)
+{
+    $pdo = pdoSqlConnect();
+    $query = "SELECT EXISTS(
+                            SELECT *
+                              FROM current_movies
+                             WHERE id = ?
+                               AND CURTIME() < startTime) AS exist";
+
+    $st = $pdo->prepare($query);
+    $st->execute([$movieTimeId]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st = null;
+    $pdo = null;
+
+    return intval($res[0]["exist"]);
+}
+
+
+
